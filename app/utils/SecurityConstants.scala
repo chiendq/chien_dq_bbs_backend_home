ba005398420a8092cf2d467a2a2365412ca1ec9c
chiendq <chiendq@gmail.com>
2022-07-24 22:26:23 +0700

    create secure constants

--- app/utils/SecurityConstants.scala
+++
@@ -1,8 +0,0 @@
-package utils
-
-object SecurityConstants {
-  val SECRET = "SecretToSignJWT"
-  val EXPIRATION_TIME = 1800000 // 30 minutes
-  val TOKEN_PREFIX = "Bearer "
-  val HEADER_STRING = "Authorization"
-}
